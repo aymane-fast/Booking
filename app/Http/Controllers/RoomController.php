01e0380fb15e671b4aaaa5b0f72aaea4d460da88
soufianeja <jalilsoufiane2018@gmail.com>
2024-03-19 13:50:59 +0000

    fix errors

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -19,7 +19,25 @@ class RoomController extends Controller
 
     public function show(Room $room ,$date = null)
     {
-        $days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+        if ($date) {
+            $date = new DateTime($date);
+        } else {
+            $date = new DateTime();
+        }
+    
+        $startOfWeek = (clone $date)->modify('Monday this week');
+        $endOfWeek = (clone $date)->modify('Sunday this week');
+    
+        $days = [];
+        for ($i = clone $startOfWeek; $i <= $endOfWeek; $i->modify('+1 day')) {
+            $days[] = $i->format('Y-m-d');
+        }
+    
+        $prevWeek = (clone $startOfWeek)->modify('-1 week')->format('Y-m-d');
+        $nextWeek = (clone $startOfWeek)->modify('+1 week')->format('Y-m-d');
+
+
         $sessions = Session::where('room_id', $room->id)->get();
 
         $reservations = [];
