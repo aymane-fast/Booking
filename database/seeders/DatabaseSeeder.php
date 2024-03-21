f70317c8c1c78675afd53f0739c46a0cce8a196c
soufianeja <jalilsoufiane2018@gmail.com>
2024-03-21 12:17:26 +0000

    admin and rooms seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
+// use Database\Seeders\AdminUserSeeder;
+// use Database\Seeders\RoomSeeder;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,12 +17,42 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+    {   
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'lname' => 'User',
+            'departement' => 'Admin Department',
+            'ppr' => 'adminppr',
+            'email' => 'admin@example.com',
+            'role' => 'admin',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        // Insert amphitheaters
+        for ($i = 1; $i <= 5; $i++) {
+            DB::table('rooms')->insert([
+                'room_name' => 'Amphi ' . $i,
+                'capacity' => 600,
+            ]);
+        }
+
+        // Insert "Salle M" rooms
+        for ($i = 1; $i <= 12; $i++) {
+            DB::table('rooms')->insert([
+                'room_name' => 'Salle M ' . $i,
+                'capacity' => 100,
+            ]);
+        }
+
+        // Insert other rooms
+        $otherRooms = ['Salle C 1', 'Salle G 1', 'Salle P 1'];
+        foreach ($otherRooms as $roomName) {
+            DB::table('rooms')->insert([
+                'room_name' => $roomName,
+                'capacity' => 100,
+            ]);
+        }
+
     }
 }
